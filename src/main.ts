39f3894c98b9af330555c5fb9b368298d9e121c4
r2 <jbrayden35@gmail.com>
2023-11-04 17:13:51 -0700

    step b complete: added center with sensor, start at null island, added unique IDs

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,51 @@ import luck from "./luck";
 import "./leafletWorkaround";
 
 
-const MERRILL_CLASSROOM = leaflet.latLng({
-    lat: 36.9995,
-    lng: - 122.0533
-});
+class CoordinateConverter {
+    coordinatesMap: Map<string, { i: number; j: number }>;
+
+    constructor() {
+        this.coordinatesMap = new Map();
+    }
+
+    convertToGameCell(latitude: number, longitude: number) {
+        const i = Math.floor(latitude * 1e4);
+        const j = Math.floor(longitude * 1e4);
+
+        if (this.coordinatesMap.has(`${i}:${j}`)) {
+            return this.coordinatesMap.get(`${i}:${j}`);
+        }
+
+        this.coordinatesMap.set(`${i}:${j}`, { i, j });
+        return { i, j };
+    }
+}
+
+// Define the base coordinates of Null Island
+const NULL_ISLAND = {
+    lat: 0,
+    lng: 0
+};
+
+// const MERRILL_CLASSROOM = leaflet.latLng({
+//     lat: 36.9995,
+//     lng: - 122.0533
+// });
+
+// const UTC = leaflet.latLng({
+//     lat: 36.97191,
+//     lng: - 122.02593
+// });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
+const TILE_DEGREES = 0.0001;  //1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-    center: MERRILL_CLASSROOM,
+    center: NULL_ISLAND,
     zoom: GAMEPLAY_ZOOM_LEVEL,
     minZoom: GAMEPLAY_ZOOM_LEVEL,
     maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -31,15 +62,22 @@ leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
 }).addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+const playerMarker = leaflet.marker(NULL_ISLAND); //merril
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
 const sensorButton = document.querySelector("#sensor")!;
 sensorButton.addEventListener("click", () => {
     navigator.geolocation.watchPosition((position) => {
-        playerMarker.setLatLng(leaflet.latLng(position.coords.latitude, position.coords.longitude));
-        map.setView(playerMarker.getLatLng());
+        const playerLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        playerMarker.setLatLng(playerLocation);
+        map.setView(playerLocation);
+        /////////
+        makeCells(playerLocation);
     });
 });
    
@@ -49,20 +87,24 @@ statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
     const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
+        [NULL_ISLAND.lat + i * TILE_DEGREES, NULL_ISLAND.lng + j * TILE_DEGREES],
+        [NULL_ISLAND.lat + (i + 1) * TILE_DEGREES, NULL_ISLAND.lng + (j + 1) * TILE_DEGREES],
     ]);
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+    //const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+    const pit = leaflet.rectangle(bounds, {
+        color: 'green',       // Change the border color to red
+        fillColor: 'green',  // Change the fill color to blue
+        fillOpacity: 0.5    // Set fill opacity (0.0 to 1.0)
+    });
 
 
     pit.bindPopup(() => {
+        const uniqueId = `${i}:${j}#${Math.floor(Math.random() * 1000)}`; // Generate a unique identifier for the pit
         let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
         const container = document.createElement("div");
         container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
+                <div>There is a pit here at i: "${i},j: ${j}". It has value <span id="value">${value}</span>. Unique ID: ${uniqueId}</div>
                 <button id="poke">poke</button>
                 <button id="stash">stash</button>`;;
         const poke = container.querySelector<HTMLButtonElement>("#poke")!;
@@ -89,10 +131,18 @@ function makePit(i: number, j: number) {
     pit.addTo(map);
 }
 
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
+function makeCells(playerLocation: { lat: number; lng: number }) {
+    const coordinateConverter = new CoordinateConverter();
+    const playerCell = coordinateConverter.convertToGameCell(playerLocation.lat, playerLocation.lng);
+    if (playerCell) {
+        for (let i = playerCell.i - NEIGHBORHOOD_SIZE; i <= playerCell.i + NEIGHBORHOOD_SIZE; i++) {
+            for (let j = playerCell.j - NEIGHBORHOOD_SIZE; j <= playerCell.j + NEIGHBORHOOD_SIZE; j++) {
+                if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+                    makePit(i, j);
+                }
+            }
         }
     }
 }
+
+makeCells(NULL_ISLAND);
